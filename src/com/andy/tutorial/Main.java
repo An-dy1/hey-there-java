ff9bcc926baf6a50218aacc79388d6d1753240ad
Andrea Johnson <andrea.johnson@vml.com>
2019-05-09 10:26:09 -0500

    Check out my turnary operator

--- src/com/andy/tutorial/Main.java
+++ src/com/andy/tutorial/Main.java
@@ -5,19 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
         int value1 = 100;
-        int value2 = 22;
+        int value2 = 0;
         int result;
-        char opCode = 'm';
+        char opCode = 'd';
 
         if (opCode == 'a') {
             result = value1 + value2;
         } else if (opCode == 's') {
             result = value1 - value2;
         } else if (opCode == 'd') {
-            result = value1 / value2;
+            if (value2 != 0) {
+                result = value1 / value2;
+            } else {
+                result = 0;
+                System.out.println("Can't divide by 0 my friend");
+            }
+            /* Alternative:
+            * result = value2 != 0 ? value1/value2 : 0;
+            * */
         } else if (opCode == 'm') {
             result = value1 * value2;
         } else {
+            System.out.println("Error - invalid opCode");
             result = 0;
         }
 
